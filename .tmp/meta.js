aa50dcde4cb64056717a075682122d9c247e32ab
l704863851 <704863851@qq.com>
2023-05-12 09:19:33 +0800

    打包

--- .tmp/meta.js
+++ .tmp/meta.js
@@ -163,7 +163,7 @@ const componentList = generateComponentList(components);
 
 export { components, componentList };
 
-const execCompile = !!false;
+const execCompile = !!true;
 
 if (!execCompile) {
   window.LowcodeRglDemoMeta = { components, componentList };
